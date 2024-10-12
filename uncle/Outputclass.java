38a5a01efc33e353cf4f39fe806442e2cea6033e
ashishbasnet-0120 <basnetashish@gmail.com>
2024-10-12 22:38:16 +0545

    this is for taking input and finding out the output in java

---
+++ uncle/Outputclass.java
@@ -0,0 +1,11 @@
+public class Outputclass {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.print("****\n");
+        System.out.print("*****");
+        System.out.println("Hello");
+    }
+    
+}
